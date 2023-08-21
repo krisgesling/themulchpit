4e0f551c9a3b7cf4040bef1e7a2832e023b43028
Kris Gesling <krisgesling@gmail.com>
2023-08-21 22:43:48 +0930

    Test deployment

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,45 @@
+<script>
+	import '../app.css';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import { Footer, FooterCopyright, FooterLinkGroup, FooterLink } from 'flowbite-svelte';
+    let copyrightMessage = ""
+</script>
+
+<header class="bg-white">
+	<div class="mx-auto max-w-7xl py-2 sm:px-6 lg:px-8">
+		<Navbar let:hidden let:toggle>
+			<NavBrand href="/">
+				<img
+					src="/images/uca-logo_no-bg.png"
+					class="mr-3 h-6 sm:h-9"
+					alt="Uniting Church of Australia Logo"
+				/>
+				<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+					>The Mulch Pit</span
+				>
+			</NavBrand>
+			<NavHamburger on:click={toggle} />
+			<NavUl {hidden}>
+				<NavLi href="/">Home</NavLi>
+				<NavLi href="/about">About</NavLi>
+				<NavLi href="/blog">Blog</NavLi>
+				<NavLi href="/events">Events</NavLi>
+				<NavLi href="/contact">Contact</NavLi>
+			</NavUl>
+		</Navbar>
+	</div>
+</header>
+
+<slot />
+
+<Footer>
+	<FooterCopyright href="/" by="Uniting Church of Australia" year={2023} {copyrightMessage} />
+	<FooterLinkGroup
+		ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
+	>
+		<FooterLink href="/">About</FooterLink>
+		<FooterLink href="/">Privacy Policy</FooterLink>
+		<FooterLink href="/">Licensing</FooterLink>
+		<FooterLink href="/">Contact</FooterLink>
+	</FooterLinkGroup>
+</Footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Banner } from 'flowbite-svelte';
+	import { Icon } from 'flowbite-svelte-icons';
+	import { Carousel } from 'flowbite-svelte';
+	import { images } from './imageData/+server.js';
+
+	let showThumbs = false;
+	let showCaptions = false;
+</script>
+
+<div class="bg-white">
+	<Banner id="default-banner">
+		<p class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+			<span class="inline-flex p-1 mr-3 bg-gray-200 rounded-full dark:bg-gray-600">
+				<Icon name="bullhorn-solid" class="w-3 h-3 text-gray-500 dark:text-gray-400" />
+				<span class="sr-only">Light bulb</span>
+			</span>
+			<span>
+				The Mulch Pit website has been pruned, ready for new growth!
+			</span>
+		</p>
+	</Banner>
+	<div class="mx-auto max-w-7xl py-2 sm:px-32 lg:px-32">
+		<!-- <div class="max-w-4xl"> -->
+		<Carousel {images} loop {showCaptions} {showThumbs} duration="3000" />
+		<!-- </div> -->
+	</div>
+</div>
